a7ca32c5af7365c2ec7e5adf232871d7466d709d
Bhumi1084 <151613011+Bhumi1084@users.noreply.github.com>
2024-12-02 17:39:07 +0530

    odoo_map
    2/12/24

---
+++ odoo_map_git/custom_addons/ajscript/models/contact.py
@@ -0,0 +1,10 @@
+from odoo import models, fields
+
+class AJsContact(models.Model):
+    _name = 'ajs.contact'
+    _description = 'AJS Contact'
+
+    name = fields.Char()
+    age = fields.Integer()
+    latitude = fields.Float(digits=(10,7))
+    longitude = fields.Float(digits=(10,7))
